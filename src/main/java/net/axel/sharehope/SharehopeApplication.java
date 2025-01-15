3e28b3c0c06d5127f8142970dee13b813d506429
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-15 03:46:52 +0100

    fc

---
+++ src/main/java/net/axel/sharehope/SharehopeApplication.java
@@ -0,0 +1,13 @@
+package net.axel.sharehope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharehopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharehopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/axel/sharehope/SharehopeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.axel.sharehope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharehopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
