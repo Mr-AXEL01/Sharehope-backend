eeb20df9925d58f5808f55f8f2cbd28ba3748c0e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-15 16:56:12 +0100

    Feat : add enums
    set new enum that represent the status of each action : Donation || Need .

---
+++ src/main/java/net/axel/sharehope/domain/enums/DonationStatus.java
@@ -0,0 +1,7 @@
+package net.axel.sharehope.domain.enums;
+
+public enum DonationStatus {
+    PENDING,
+    CONFIRMED,
+    DELIVERED
+}
---
+++ src/main/java/net/axel/sharehope/domain/enums/NeedStatus.java
@@ -0,0 +1,7 @@
+package net.axel.sharehope.domain.enums;
+
+public enum NeedStatus {
+    PENDING,
+    CONFIRMED,
+    REJECTED
+}
