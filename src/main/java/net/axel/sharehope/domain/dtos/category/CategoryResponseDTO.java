84b29faad3c7f5679c591a22ed32bdfb0199b3b1
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-17 16:46:19 +0100

    SHAR-39 Feat(category): add projection for category entity
    Created a projection interface for the Category entity to optimize data retrieval by exposing only necessary fields. This improves performance and reduces unnecessary data transfer.

---
+++ src/main/java/net/axel/sharehope/domain/dtos/category/CategoryResponseDTO.java
@@ -0,0 +1,30 @@
+package net.axel.sharehope.domain.dtos.category;
+
+import java.time.Instant;
+import java.util.List;
+
+/**
+ * Projection for {@link net.axel.sharehope.domain.entities.Category}
+ */
+public interface CategoryResponseDTO {
+    Long getId();
+
+    String getCategoryName();
+
+    String getDescription();
+
+    List<ActionINestedDTO> getActions();
+
+    /**
+     * Projection for {@link net.axel.sharehope.domain.entities.Action}
+     */
+    interface ActionINestedDTO {
+        Long getId();
+
+        Double getAmount();
+
+        String getDescription();
+
+        Instant getCreatedAt();
+    }
+}
