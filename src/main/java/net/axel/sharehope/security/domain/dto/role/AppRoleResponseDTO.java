fcb101c37e03a7446fc3476d7e4a976eff80be6b
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-17 17:17:57 +0100

    SHAR-43 Feat(role): Add projection for the AppRole entity
    Created a projection interface for the AppRole entity to optimize data retrieval by exposing only necessary fields. This improves performance and reduces unnecessary data transfer.

---
+++ src/main/java/net/axel/sharehope/security/domain/dto/role/AppRoleResponseDTO.java
@@ -0,0 +1,10 @@
+package net.axel.sharehope.security.domain.dto.role;
+
+/**
+ * Projection for {@link net.axel.sharehope.security.domain.entity.AppRole}
+ */
+public interface AppRoleResponseDTO {
+    Long getId();
+
+    String getRole();
+}
---
+++ src/main/java/net/axel/sharehope/security/domain/dto/user/AppUserResponseDTO.java
@@ -0,0 +1,44 @@
+package net.axel.sharehope.security.domain.dto.user;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Projection for {@link net.axel.sharehope.security.domain.entity.AppUser}
+ */
+public interface AppUserResponseDTO {
+    Long getId();
+
+    String getUsername();
+
+    String getEmail();
+
+    String getPhone();
+
+    Set<AppRoleNestedDTO> getRoles();
+
+    List<ArticleNestedDTO> getArticles();
+
+    /**
+     * Projection for {@link net.axel.sharehope.security.domain.entity.AppRole}
+     */
+    interface AppRoleNestedDTO {
+        String getRole();
+    }
+
+    /**
+     * Projection for {@link net.axel.sharehope.domain.entities.Article}
+     */
+    interface ArticleNestedDTO {
+        Long getId();
+
+        String getTitle();
+
+        String getDescription();
+
+        String getContent();
+
+        Instant getCreatedAt();
+    }
+}
