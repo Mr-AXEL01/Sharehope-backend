db73219a6febf7e71c3364056cd61b7e87311cdd
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-18 21:55:30 +0100

    SHAR-55 Feat(user): add mapper for AppUser entity
    Implemented a mapper for the AppUser entity to facilitate conversion between DTOs and entity objects. This improves code maintainability and data transformation consistency.

---
+++ src/main/java/net/axel/sharehope/mapper/AppUserMapper.java
@@ -0,0 +1,7 @@
+package net.axel.sharehope.mapper;
+
+import net.axel.sharehope.security.domain.dto.user.UserRequestDTO;
+import net.axel.sharehope.security.domain.entity.AppUser;
+
+public interface AppUserMapper extends BaseMapper<AppUser, UserRequestDTO> {
+}
