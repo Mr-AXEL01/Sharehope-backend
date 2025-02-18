3e6128c43a0741b0c79e8f8d4435d33e8aeab8a5
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-18 21:51:29 +0100

    SHAR-56 Feat(article): add mapper for article entity
    Implemented a mapper for the rticle entity to facilitate conversion between DTOs and entity objects. This improves code maintainability and data transformation consistency.

---
+++ src/main/java/net/axel/sharehope/mapper/ArticleMapper.java
@@ -0,0 +1,7 @@
+package net.axel.sharehope.mapper;
+
+import net.axel.sharehope.domain.dtos.article.ArticleRequestDTO;
+import net.axel.sharehope.domain.entities.Article;
+
+public interface ArticleMapper extends BaseMapper<Article, ArticleRequestDTO> {
+}
