5dae40fe126033353ebdfbd4caddbf48cfef560e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-18 16:03:08 +0100

    SHAR-52 Feat(user): Add request DTO for AppUser
    Created request DTO to standardize input data for AppUser. This improves data validation, security, and API consistency.

---
+++ src/main/java/net/axel/sharehope/security/domain/dto/user/UserRequestDTO.java
@@ -0,0 +1,19 @@
+package net.axel.sharehope.security.domain.dto.user;
+
+import jakarta.validation.constraints.NotBlank;
+import net.axel.sharehope.security.domain.entity.AppUser;
+import net.axel.sharehope.validation.IsUnique;
+import org.springframework.web.multipart.MultipartFile;
+
+public record UserRequestDTO(
+
+        @IsUnique(entityClass = AppUser.class, fieldName = "email", message = "email already taken.")
+        @NotBlank String email,
+
+        @NotBlank String password,
+
+        @NotBlank String phone,
+
+        MultipartFile avatar
+) {
+}
