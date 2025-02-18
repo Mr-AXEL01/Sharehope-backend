3bb0aba65f1f400fa4ca6224a4a7609a2187896d
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-18 15:23:18 +0100

    SHAR-45 Feat(action): Add request DTO for action (Donation && need)
    Created request DTO to standardize input data for Donation and Need actions. This improves data validation, security, and API consistency.

---
+++ src/main/java/net/axel/sharehope/domain/dtos/action/ActionRequestDTO.java
@@ -0,0 +1,23 @@
+package net.axel.sharehope.domain.dtos.action;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import net.axel.sharehope.domain.entities.Category;
+import net.axel.sharehope.validation.Exists;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public record ActionRequestDTO(
+
+        @Positive Double amount,
+
+        @NotBlank String description,
+
+        @Exists(entityClass = Category.class, fieldName = "id", message = "Category does not exists.")
+        @NotNull Long categoryId,
+
+        List<MultipartFile> attachments
+) {
+}
