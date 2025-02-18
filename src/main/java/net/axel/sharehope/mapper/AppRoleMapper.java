87f2d39fd5a73551639dcc258c18c5cb67d1e86d
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-18 21:54:26 +0100

    SHAR-54 Feat(role): add mapper for Approle entity
    Implemented a mapper for the AppRole entity to facilitate conversion between DTOs and entity objects. This improves code maintainability and data transformation consistency.

---
+++ src/main/java/net/axel/sharehope/mapper/AppRoleMapper.java
@@ -0,0 +1,7 @@
+package net.axel.sharehope.mapper;
+
+import net.axel.sharehope.security.domain.dto.role.RoleRequestDTO;
+import net.axel.sharehope.security.domain.entity.AppRole;
+
+public interface AppRoleMapper extends BaseMapper<AppRole, RoleRequestDTO>{
+}
