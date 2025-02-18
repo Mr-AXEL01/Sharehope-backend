0b85f1780706f2b822a45cdb29c3227f20088bc1
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-18 21:43:09 +0100

    SHAR-57 Feat(category): Add mapper for category entity
    Implemented a mapper for the Category entity to facilitate conversion between DTOs and entity objects. This improves code maintainability and data transformation consistency.

---
+++ src/main/java/net/axel/sharehope/mapper/CategoryMapper.java
@@ -0,0 +1,7 @@
+package net.axel.sharehope.mapper;
+
+import net.axel.sharehope.domain.dtos.category.CategoryResponseDTO;
+import net.axel.sharehope.domain.entities.Category;
+
+public interface CategoryMapper extends BaseMapper<Category, CategoryResponseDTO> {
+}
