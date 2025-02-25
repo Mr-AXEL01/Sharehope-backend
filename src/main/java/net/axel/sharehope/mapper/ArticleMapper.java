0cc795909b3e259446787ef60d6223788ec89e53
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-25 14:11:42 +0000

    Docs(Article): change the requestDto into createArticleDTO

--- src/main/java/net/axel/sharehope/mapper/ArticleMapper.java
+++ src/main/java/net/axel/sharehope/mapper/ArticleMapper.java
@@ -1,10 +1,10 @@
 package net.axel.sharehope.mapper;
 
-import net.axel.sharehope.domain.dtos.article.ArticleRequestDTO;
+import net.axel.sharehope.domain.dtos.article.CreateArticleDTO;
 import net.axel.sharehope.domain.dtos.article.ArticleResponseDTO;
 import net.axel.sharehope.domain.entities.Article;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public interface ArticleMapper extends BaseMapper<Article, ArticleRequestDTO, ArticleResponseDTO> {
+public interface ArticleMapper extends BaseMapper<Article, CreateArticleDTO, ArticleResponseDTO> {
 }
