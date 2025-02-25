f5f90bb2f0da883236189c8bf90d79b73a06e3dd
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-25 14:14:41 +0000

    Feat(Article): add custom queries
    set custom queries :
     - find an article by it's ID and return an article projection direct to
       optimise the performance of the query + avoid exposing the entity.
     - find all articles in an order way depend on the last created , and
       also returned as projection.

--- src/main/java/net/axel/sharehope/repository/ArticleRepository.java
+++ src/main/java/net/axel/sharehope/repository/ArticleRepository.java
@@ -1,7 +1,17 @@
 package net.axel.sharehope.repository;
 
+import net.axel.sharehope.domain.dtos.article.ArticleProjectionDTO;
 import net.axel.sharehope.domain.entities.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    Page<ArticleProjectionDTO> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    Optional<ArticleProjectionDTO> findArticleById(Long id);
 }
