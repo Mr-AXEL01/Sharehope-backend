cd8cd8acda8128dc1f80f746316dc3916597f02b
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-26 09:45:19 +0000

    Feat(core): Initialize default role and admin user
    - Added commandLineRunner to initialize ROLE_USER and ROLE_ADMIN if not present.
    - Created an admin user with default credentials if no users exist.
    - Ensured roles are fetched safely with error handling for missing entries.

--- src/main/java/net/axel/sharehope/SharehopeApplication.java
+++ src/main/java/net/axel/sharehope/SharehopeApplication.java
@@ -1,13 +1,58 @@
 package net.axel.sharehope;
 
+import net.axel.sharehope.exception.domains.ResourceNotFoundException;
+import net.axel.sharehope.security.domain.entity.AppRole;
+import net.axel.sharehope.security.domain.entity.AppUser;
+import net.axel.sharehope.security.repository.AppRoleRepository;
+import net.axel.sharehope.security.repository.AppUserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class SharehopeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SharehopeApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(AppUserRepository userRepository, AppRoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.count() == 0) {
+                AppRole roleUser = new AppRole();
+                roleUser.setRole("ROLE_USER");
+                AppRole roleAdmin = new AppRole();
+                roleAdmin.setRole("ROLE_ADMIN");
+                roleRepository.save(roleUser);
+                roleRepository.save(roleAdmin);
+            }
+
+            if (userRepository.count() == 0) {
+                AppUser adminUser = new AppUser();
+                adminUser.setUsername("admin")
+                        .setEmail("admin@wora.com")
+                        .setPassword("admin123")
+                        .setPhone("0639330455");
+
+                Set<AppRole> roles = new HashSet<>();
+                roles.add(roleRepository.findByRole("ROLE_ADMIN")
+                        .orElseThrow(()-> new ResourceNotFoundException("Role admin not exists."))
+                );
+
+                roles.add(roleRepository.findByRole("ROLE_USER")
+                        .orElseThrow(()-> new ResourceNotFoundException("Role User not exists."))
+                );
+
+                adminUser.setRoles(roles);
+
+                userRepository.save(adminUser);
+            }
+        };
     }
 
 }
