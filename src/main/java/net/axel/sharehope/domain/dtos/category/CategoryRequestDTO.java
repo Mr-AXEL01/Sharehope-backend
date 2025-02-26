01249000ace6dff88ee3045e24ee279dfe4afab7
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-26 14:47:57 +0000

    add isUnique anootation to the categoryName in the requestDTO
    set the annoation isUnique to check that there is no exist category with provided name.

--- src/main/java/net/axel/sharehope/domain/dtos/category/CategoryRequestDTO.java
+++ src/main/java/net/axel/sharehope/domain/dtos/category/CategoryRequestDTO.java
@@ -1,10 +1,13 @@
 package net.axel.sharehope.domain.dtos.category;
 
 import jakarta.validation.constraints.NotBlank;
+import net.axel.sharehope.domain.entities.Category;
+import net.axel.sharehope.validation.IsUnique;
 import org.springframework.web.multipart.MultipartFile;
 
 public record CategoryRequestDTO(
 
+        @IsUnique(entityClass = Category.class, fieldName = "categoryName", message = "There is already a category with this name!")
         @NotBlank String categoryName,
 
         @NotBlank String description,
