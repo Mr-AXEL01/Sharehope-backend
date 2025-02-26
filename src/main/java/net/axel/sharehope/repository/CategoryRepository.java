887032b2acfc370fd3fe93c92ec39588a5655b8a
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-26 16:23:24 +0000

    Feat(Category): set custom queries for category
    set custome queries to :
    - get all the category in oder of the last inserted.
    - get a spesific category.
    - set the return type to be projection to avoid exposing the category entity.

--- src/main/java/net/axel/sharehope/repository/CategoryRepository.java
+++ src/main/java/net/axel/sharehope/repository/CategoryRepository.java
@@ -1,7 +1,15 @@
 package net.axel.sharehope.repository;
 
+import net.axel.sharehope.domain.dtos.category.CategoryProjectionDTO;
 import net.axel.sharehope.domain.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-  }
+
+    List<CategoryProjectionDTO> findAllCategoryByOrderByIdDesc();
+
+    Optional<CategoryProjectionDTO> findCategoryById(Long id);
+}
