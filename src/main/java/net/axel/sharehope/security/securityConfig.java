08524c34e4edb6c7ea16ac716f6e8f51be9e34b8
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-28 19:43:37 +0000

    Feat(config): add bean for password enconder , userDetailsService
    - set bean for password encoder and use password encoder factories .
    - set the customUserDetailsService for the bean of the user detail service.

--- src/main/java/net/axel/sharehope/security/securityConfig.java
+++ src/main/java/net/axel/sharehope/security/securityConfig.java
@@ -1,26 +1,50 @@
 package net.axel.sharehope.security;
 
+import lombok.RequiredArgsConstructor;
+import net.axel.sharehope.security.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+
+@RequiredArgsConstructor
 public class securityConfig {
+
+    private static final String PREFIX = "/api/v1";
+
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(PREFIX+"/", PREFIX+"/register", PREFIX+"/login").permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .logout(LogoutConfigurer::permitAll)
                 .build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return customUserDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
