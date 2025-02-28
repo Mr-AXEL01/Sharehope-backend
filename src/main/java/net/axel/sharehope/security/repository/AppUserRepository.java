f99075c123d05e02e8af6a69c2457b4b6e74c37c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-02-28 17:02:53 +0000

    Feat(AppUser): add new method to get user
     - get user by username.
     - use Otional to avoid havving null.

--- src/main/java/net/axel/sharehope/security/repository/AppUserRepository.java
+++ src/main/java/net/axel/sharehope/security/repository/AppUserRepository.java
@@ -3,5 +3,9 @@ package net.axel.sharehope.security.repository;
 import net.axel.sharehope.security.domain.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByUsername(String username);
 }
