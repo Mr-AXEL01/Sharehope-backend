142b364543428465cfc226776f6ecd8b0f0d3d33
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-02 15:44:22 +0000

    Feat(mapper): annotate the mappers with @Mapper
    annotate the mappers with @Mapper and set the componentModel to be spring , so spring will handle it.

--- src/main/java/net/axel/sharehope/mapper/AppRoleMapper.java
+++ src/main/java/net/axel/sharehope/mapper/AppRoleMapper.java
@@ -3,6 +3,8 @@ package net.axel.sharehope.mapper;
 import net.axel.sharehope.security.domain.dto.role.RoleRequestDTO;
 import net.axel.sharehope.security.domain.dto.role.RoleResponseDTO;
 import net.axel.sharehope.security.domain.entity.AppRole;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface AppRoleMapper extends BaseMapper<AppRole, RoleRequestDTO, RoleResponseDTO>{
 }
--- src/main/java/net/axel/sharehope/mapper/AppUserMapper.java
+++ src/main/java/net/axel/sharehope/mapper/AppUserMapper.java
@@ -1,8 +1,10 @@
 package net.axel.sharehope.mapper;
 
-import net.axel.sharehope.security.domain.dto.user.UserRequestDTO;
+import net.axel.sharehope.security.domain.dto.user.UserRegisterDTO;
 import net.axel.sharehope.security.domain.dto.user.UserResponseDTO;
 import net.axel.sharehope.security.domain.entity.AppUser;
+import org.mapstruct.Mapper;
 
-public interface AppUserMapper extends BaseMapper<AppUser, UserRequestDTO, UserResponseDTO> {
+@Mapper(componentModel = "spring")
+public interface AppUserMapper extends BaseMapper<AppUser, UserRegisterDTO, UserResponseDTO> {
 }
