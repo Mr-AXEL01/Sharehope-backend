9c9551d55a40d789b6dde5bb6643f909ddf5d21d
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-04 15:42:38 +0000

    Feat(Attachment): add new method to find an attachment
    set a new behaviour to find an attachment by the attachableType && attachableId.

--- src/main/java/net/axel/sharehope/repository/AttachmentRepository.java
+++ src/main/java/net/axel/sharehope/repository/AttachmentRepository.java
@@ -4,4 +4,6 @@ import net.axel.sharehope.domain.entities.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+
+    Attachment findByAttachableTypeAndAttachableId(String attachableType, Long attachableId);
 }
