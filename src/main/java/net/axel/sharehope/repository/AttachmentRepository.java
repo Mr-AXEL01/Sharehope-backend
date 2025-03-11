8c5a5a927f9b81c4a36f3e6678c5d5c0da676590
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-11 06:29:18 +0000

    Refactor(Attachment): change the find method to be optional

--- src/main/java/net/axel/sharehope/repository/AttachmentRepository.java
+++ src/main/java/net/axel/sharehope/repository/AttachmentRepository.java
@@ -3,7 +3,9 @@ package net.axel.sharehope.repository;
 import net.axel.sharehope.domain.entities.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
 
-    Attachment findByAttachableTypeAndAttachableId(String attachableType, Long attachableId);
+    Optional<Attachment> findByAttachableTypeAndAttachableId(String attachableType, Long attachableId);
 }
