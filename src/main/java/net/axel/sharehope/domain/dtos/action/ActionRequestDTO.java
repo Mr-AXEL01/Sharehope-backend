9656d753dc5f5d67729908b0539c2451e59d1354
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-12 17:14:00 +0000

    Docs(ActionRequestDTO): change the validation annotation
    update the validation annotation from @positive -> positiveOrZero , so we can accept the 0.

--- src/main/java/net/axel/sharehope/domain/dtos/action/ActionRequestDTO.java
+++ src/main/java/net/axel/sharehope/domain/dtos/action/ActionRequestDTO.java
@@ -2,7 +2,7 @@ package net.axel.sharehope.domain.dtos.action;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import net.axel.sharehope.domain.entities.Category;
 import net.axel.sharehope.validation.Exists;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,7 +11,7 @@ import java.util.List;
 
 public record ActionRequestDTO(
 
-        @Positive Double amount,
+        @PositiveOrZero Double amount,
 
         @NotBlank String description,
 
