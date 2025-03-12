117bfc93a82cc86660fc9c840429debd39143a71
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-12 17:33:43 +0000

    Feat(Donatipn): add response DTO for the donation

---
+++ src/main/java/net/axel/sharehope/domain/dtos/action/donation/DonationResponseDTO.java
@@ -0,0 +1,20 @@
+package net.axel.sharehope.domain.dtos.action.donation;
+
+import net.axel.sharehope.domain.dtos.category.CategoryEmbeddedDTO;
+import net.axel.sharehope.domain.enums.DonationStatus;
+import net.axel.sharehope.security.domain.dto.user.UserEmbeddedDTO;
+
+import java.time.Instant;
+import java.util.List;
+
+public record DonationResponseDTO(
+        Long id,
+        Double amount,
+        String description,
+        DonationStatus donationStatus,
+        CategoryEmbeddedDTO category,
+        UserEmbeddedDTO user,
+        Instant createdAt,
+        List<String> attachmentUrls
+) {
+}
