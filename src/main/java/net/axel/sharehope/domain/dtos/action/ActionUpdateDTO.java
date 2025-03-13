efb9c22c60c475629009e6778aed4e8fcac2dbc2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-13 11:25:37 +0000

    Feat(actions): set request DTOs
    define DTO for the create and other one for the update.

--- src/main/java/net/axel/sharehope/domain/dtos/action/ActionUpdateDTO.java
+++ src/main/java/net/axel/sharehope/domain/dtos/action/ActionUpdateDTO.java
@@ -1,19 +1,14 @@
 package net.axel.sharehope.domain.dtos.action;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import net.axel.sharehope.domain.entities.Category;
 import net.axel.sharehope.validation.Exists;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-public record ActionRequestDTO(
-
-        @PositiveOrZero Double amount,
-
-        @NotBlank String description,
+public record ActionUpdateDTO(
+        String description,
 
         @Exists(entityClass = Category.class, fieldName = "id", message = "Category does not exists.")
         @NotNull Long categoryId,
