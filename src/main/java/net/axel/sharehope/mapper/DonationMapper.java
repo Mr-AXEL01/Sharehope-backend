c15abbc413d8dcc88ee3f02718547b590b982178
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-13 11:26:34 +0000

    Feat(Donation): define a custom mapper for the donation

--- src/main/java/net/axel/sharehope/mapper/DonationMapper.java
+++ src/main/java/net/axel/sharehope/mapper/DonationMapper.java
@@ -1,11 +1,45 @@
 package net.axel.sharehope.mapper;
 
-import net.axel.sharehope.domain.dtos.action.ActionRequestDTO;
 import net.axel.sharehope.domain.dtos.action.donation.DonationResponseDTO;
+import net.axel.sharehope.domain.dtos.category.CategoryEmbeddedDTO;
+import net.axel.sharehope.domain.entities.Category;
 import net.axel.sharehope.domain.entities.Donation;
-import org.mapstruct.Mapper;
+import net.axel.sharehope.security.domain.dto.user.UserEmbeddedDTO;
+import net.axel.sharehope.security.domain.entity.AppUser;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface DonationMapper extends BaseMapper<Donation, ActionRequestDTO, DonationResponseDTO> {
+@Component
+public class DonationMapper {
 
+    public DonationResponseDTO toResponse(Donation donation) {
+//        System.out.println("\n============" + donation.getUser().getAvatar()+ "=========\n");
+        return new DonationResponseDTO(
+                donation.getId(),
+                donation.getAmount(),
+                donation.getDescription(),
+                donation.getDonationStatus(),
+                mapCategory(donation.getCategory()),
+                mapUser(donation.getUser()),
+                donation.getCreatedAt(),
+                donation.getAttachments()
+        );
+    }
+
+    private CategoryEmbeddedDTO mapCategory(Category category) {
+        return new CategoryEmbeddedDTO(
+                category.getId(),
+                category.getCategoryName(),
+                category.getDescription()
+        );
+    }
+
+    private UserEmbeddedDTO mapUser(AppUser user) {
+        return new UserEmbeddedDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPhone(),
+                user.getAvatar()
+        );
+    }
 }
