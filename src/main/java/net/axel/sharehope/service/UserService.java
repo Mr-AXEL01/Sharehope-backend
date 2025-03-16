10c9b0e2a8165d31b762c6a9bb9067241a270585
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-16 17:52:26 +0000

    Feat(USER): add updateAvatar()
    set up the update avatar method , where we replace the existing avatar with new one and remove it from cloud , and if the new one not feat for our ruls we asign default one.

--- src/main/java/net/axel/sharehope/service/UserService.java
+++ src/main/java/net/axel/sharehope/service/UserService.java
@@ -1,8 +1,19 @@
 package net.axel.sharehope.service;
 
+import net.axel.sharehope.security.domain.dto.user.UserResponseDTO;
+import net.axel.sharehope.security.domain.dto.user.UserUpdateDTO;
 import net.axel.sharehope.security.domain.entity.AppUser;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
 
+    UserResponseDTO findById(Long id);
+
     AppUser findUserEntity(String username);
+
+    UserResponseDTO updateAvatar(Long id, MultipartFile avatarUrl);
+
+    UserResponseDTO update(Long id, UserUpdateDTO updateDTO);
+
+    UserResponseDTO updatePassword(Long id, String password);
 }
--- src/main/java/net/axel/sharehope/service/impl/UserServiceImpl.java
+++ src/main/java/net/axel/sharehope/service/impl/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package net.axel.sharehope.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import net.axel.sharehope.domain.dtos.attachment.AttachmentRequestDTO;
+import net.axel.sharehope.domain.dtos.attachment.AttachmentResponseDTO;
+import net.axel.sharehope.domain.entities.Attachment;
 import net.axel.sharehope.exception.domains.ResourceNotFoundException;
 import net.axel.sharehope.mapper.UserMapper;
+import net.axel.sharehope.security.domain.dto.user.UserResponseDTO;
+import net.axel.sharehope.security.domain.dto.user.UserUpdateDTO;
 import net.axel.sharehope.security.domain.entity.AppUser;
 import net.axel.sharehope.security.repository.AppUserRepository;
 import net.axel.sharehope.security.service.Impl.JWTService;
@@ -13,6 +18,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Transactional
@@ -32,6 +38,16 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
+    public UserResponseDTO findById(Long id) {
+        AppUser user = getUser(id);
+
+        String avatar = attachmentService.findAttachmentUrl("AppUser", user.getId());
+        user.setAvatar(avatar != null ? avatar : DEFAULT_AVATAR_URL);
+
+        return mapper.mapToResponseDTO(user);
+    }
+
+    @Override
     public AppUser findUserEntity(String username) {
         AppUser user = repository.findByUsername(username)
                 .orElseThrow(() -> new ResourceNotFoundException("Can't find the user with username: " + username));
@@ -40,5 +56,44 @@ public class UserServiceImpl implements UserService {
         user.setAvatar(avatar != null ? avatar : DEFAULT_AVATAR_URL);
 
         return user;
+    }
+
+    @Override
+    public UserResponseDTO updateAvatar(Long id, MultipartFile avatarUrl) {
+        AppUser user = getUser(id);
+        Attachment existingAttachment = attachmentService.findAttachment("AppUser", user.getId());
+        if (existingAttachment != null) {
+            attachmentService.deleteAttachment(existingAttachment.getId(), "users/avatars");
+        }
+        String avatarPath;
+        if (avatarUrl != null && !avatarUrl.isEmpty()) {
+            AttachmentResponseDTO newAvatar = attachmentService.createAttachment(new AttachmentRequestDTO(
+                            avatarUrl,
+                            "AppUser",
+                            user.getId(),
+                            "users/avatars"
+                    )
+            );
+            avatarPath = newAvatar.filePath();
+        } else {
+            avatarPath = DEFAULT_AVATAR_URL;
+        }
+        user.setAvatar(avatarPath);
+        return mapper.mapToResponseDTO(user);
+    }
+
+    @Override
+    public UserResponseDTO update(Long id, UserUpdateDTO updateDTO) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDTO updatePassword(Long id, String password) {
+        return null;
+    }
+
+    private AppUser getUser(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Can't find the user with id: " + id));
     }
 }
