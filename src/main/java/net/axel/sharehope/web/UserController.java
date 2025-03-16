5881de0dad1c9baf72bbfed04e2b88a4e00291bc
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-16 23:27:08 +0000

    Feat(User): add a user controller
    set controller for the user to manage his profile.

---
+++ src/main/java/net/axel/sharehope/web/UserController.java
@@ -0,0 +1,34 @@
+package net.axel.sharehope.web;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.axel.sharehope.security.domain.dto.user.response.UserAuthResponseDTO;
+import net.axel.sharehope.security.domain.dto.user.response.UserResponseDTO;
+import net.axel.sharehope.security.domain.dto.user.requests.UserUpdateDTO;
+import net.axel.sharehope.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping(UserController.CONTROLLER_PATH)
+
+@RequiredArgsConstructor
+public class UserController {
+
+    public final static String CONTROLLER_PATH = "api/v1/profile";
+
+    private final UserService service;
+
+    @PatchMapping("/{id}/avatar")
+    public ResponseEntity<UserResponseDTO> updateAvatar(@PathVariable("id") Long id, MultipartFile avatarUrl) {
+        UserResponseDTO updatedUser = service.updateAvatar(id, avatarUrl);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserAuthResponseDTO> update(@PathVariable("id") Long id, @RequestBody @Valid UserUpdateDTO updateDTO) {
+        UserAuthResponseDTO updatedUser = service.update(id, updateDTO);
+        return ResponseEntity.ok(updatedUser);
+    }
+}
