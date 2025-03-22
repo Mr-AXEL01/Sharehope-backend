9d501095b767d57d92564b369e61197c567e613d
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-22 16:55:50 +0000

    Doc: remove regex by removing the @pattern

--- src/main/java/net/axel/sharehope/security/domain/dto/user/requests/UserPasswordUpdateDTO.java
+++ src/main/java/net/axel/sharehope/security/domain/dto/user/requests/UserPasswordUpdateDTO.java
@@ -1,7 +1,6 @@
 package net.axel.sharehope.security.domain.dto.user.requests;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserPasswordUpdateDTO(
@@ -10,10 +9,6 @@ public record UserPasswordUpdateDTO(
 
         @NotBlank
         @Size(min = 6, message = "Password must be at least 6 characters long")
-        @Pattern(
-                regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$",
-                message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-        )
         String newPassword
 ) {
 }
